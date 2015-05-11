ffa495039f4fba549a62cbbf5ef9b8eaac5cca3c
Adam Chappell <adamdchappell@gmail.com>
2015-05-10 21:13:46 -0500

    Closest numbers
    Find the pair of numbers in the array with the smallest difference.
    There can be multiple pairs with the lowest difference.
    
    Sort the array and keep track of the lowest difference and the two
    numbers that made up that pair. If you find a number with the same
    difference, add the pair to the list. If you find a pair with a lower
    difference, clear the list, add the new pair, and make this value now
    the lowest difference.

---
+++ Domains/Algorithms/Sorting/ClosestNumbers.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace ClosestNumbers
+{
+    class ClosestNumbers
+    {
+        static void Main(string[] args)
+        {
+            int numElements = Int32.Parse(Console.ReadLine());
+            int[] numbers = Console.ReadLine().Split().Select(n => Int32.Parse(n)).ToArray();
+            Debug.Assert(numElements == numbers.Count() && numElements > 1);
+
+            Array.Sort(numbers);
+
+            List<int> leastDifferencePairs = new List<int>();
+            leastDifferencePairs.Add(numbers[1]);
+            leastDifferencePairs.Add(numbers[0]);
+            int leastDifference = numbers[1] - numbers[0];
+            
+            for(int i = 2; i<numbers.Count(); i++)
+            {
+                int difference = numbers[i] - numbers[i - 1];
+                if (difference > leastDifference)
+                {
+                    continue;
+                }
+                else
+                {
+                    if (difference < leastDifference)
+                    {
+                        leastDifference = difference;
+                        leastDifferencePairs.Clear();
+                    }
+
+                    leastDifferencePairs.Add(numbers[i-1]);
+                    leastDifferencePairs.Add(numbers[i]);
+                }
+            }
+
+            string outputPairs = "";
+            foreach(int n in leastDifferencePairs)
+            {
+                outputPairs += n + " ";
+            }
+            outputPairs = outputPairs.Trim();
+
+            Console.WriteLine(outputPairs);
+        }
+    }
+}
