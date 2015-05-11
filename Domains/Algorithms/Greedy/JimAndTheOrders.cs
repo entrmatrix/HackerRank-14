c5508080e6177b59faa78c859e4a22966bd6898e
Adam Chappell <adamdchappell@gmail.com>
2015-05-10 21:22:18 -0500

    Jim and the Orders
    Given a list of orders. Each order has a time t that it starts and takes
    d time to process. Print the orders in the order that they finish. If
    two orders finish at the same time, the one listed in the input first
    takes priority.
    
    Create and Order class to keep track of the order number as well as the
    start and duration time. Create an array of orders and sort them by what
    time they finish. The CompareTo function handle the case where two
    orders finish at the same time. Iterate through the sorted array
    printing out the number of each order.

---
+++ Domains/Algorithms/Greedy/JimAndTheOrders.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace JimAndTheOrders
+{
+    class JimAndTheOrders
+    {
+        static void Main(string[] args)
+        {
+            int numOrders = Int32.Parse(Console.ReadLine());
+            Order[] orders = new Order[numOrders];
+
+            for(int i = 0; i<numOrders; i++)
+            {
+                string orderInfo = Console.ReadLine();
+                int start = Int32.Parse(orderInfo.Split()[0]);
+                int process = Int32.Parse(orderInfo.Split()[1]);
+
+                orders[i] = new Order(i+1, start, process);
+            }
+
+            Array.Sort(orders);
+
+            string orderOutput = "";
+            foreach(Order o in orders)
+            {
+                orderOutput += o.Number + " ";
+            }
+            orderOutput = orderOutput.Trim();
+
+            Console.WriteLine(orderOutput);
+        }
+
+        class Order : IComparable<Order>
+        {
+            private int _number, _startTime, _procTime;
+
+            public int Number
+            {
+                get { return _number; }
+            }
+
+            public Order(int num, int startT, int procT)
+            {
+                _number = num;
+                _startTime = startT;
+                _procTime = procT;
+            }
+
+
+            public int CompareTo(Order other)
+            {
+                if ((this._startTime + this._procTime) == (other._startTime + other._procTime))
+                {
+                    return this._number - other._number;
+                }
+                else
+                {
+                    return (this._startTime + this._procTime) - (other._startTime + other._procTime);
+                }
+            }
+        }
+    }
+}
