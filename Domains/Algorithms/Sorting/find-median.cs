6684fb50e7befe482282cc43c0861e5af705d8c2
Adam Chappell <adamdchappell@gmail.com>
2015-05-10 21:11:01 -0500

    Find the median
    Yes, this is as simple as it sounds. Find the median of an array of
    integers. Number of elements in the array guaranteed to be odd. All you
    need to do is sort the array and get the element in the middle.

---
+++ Domains/Algorithms/Sorting/find-median.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace find_median
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numElements = Int32.Parse(Console.ReadLine());
+            int[] numbers = Console.ReadLine().Split().Select(n => Int32.Parse(n)).ToArray();
+            Debug.Assert(numElements == numbers.Count());
+
+            Array.Sort(numbers);
+            //problem guarantees numElements is odd
+            Console.WriteLine(numbers[numElements / 2]);
+        }
+    }
+}
